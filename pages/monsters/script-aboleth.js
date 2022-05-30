34182bf4604784705c004ae20991b846f66b2825
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-30 22:07:57 +0200

    ...

--- pages/monsters/script-aboleth.js
+++ pages/monsters/script-aboleth.js
@@ -10,19 +10,58 @@ function displayAboleth(result) {
     console.log('name from objet', result.name); 
 
     const abolethArray = Object.keys(result);  //  array con chiavi e valori 
-    console.log('keyvalues', abolethArray);
+    console.log('keyvalues', abolethArray); 
+
+    const abolethTitle = document.createElement('h1'); 
+    const abolethTitleNode = document.createTextNode(result.name) 
+    abolethTitle.appendChild(abolethTitleNode); 
+    document.body.appendChild(abolethTitle);
 
     const abolethTemplate = ` 
-        <h2>#ABOLETHNAME</h2> 
+        <h2>#ABOLETHNAME:</h2> 
+        <h3 id="#ID">#DESCRIPTION</h3> 
         ` 
     const abolethContainer = document.body; 
 
-    for (const key of abolethArray) {
-        console.log('chiave:', result[1], 'valore:', result[key]);
-        const newAbolethTemplate = abolethTemplate.replace('#ABOLETHNAME', key.toUpperCase()); 
+    // for (let i = 0; i < abolethArray.length; i++) {
+    //     const element = abolethArray[i];
+    //     console.log('chiave:', abolethArray.); 
+    //             const newAbolethTemplate = abolethTemplate.replace('#ABOLETHNAME', element.toUpperCase()); 
 
+                                                
+
+    //     abolethContainer.innerHTML += newAbolethTemplate;
+    // }
+
+    for (const key of abolethArray) {
+        console.log('chiave:', key, 'valore:', result.speed); 
+        
+        console.log('speed', result.speed)
+        
+        
+        const newAbolethTemplate = abolethTemplate.replace('#ABOLETHNAME', key.toUpperCase()) 
+        .replace('#DESCRIPTION', result[key]) 
+        .replace('#ID', key);
+        
+        const speedTemplate = document.getElementById('speed'); 
+        for (const [key, value] of Object.entries(result.speed)) {
+            console.log(key, value); 
+            // const speedString = result.speed[key]
+            // const speedKeyNode = document.createTextNode(speedString) 
+            // speedTemplate.appendChild(speedKeyNode); 
+            // newAbolethTemplate.appendChild(speedTemplate);
+
+
+        }
+        
+        
+        
+
+        
+        
+        
         abolethContainer.innerHTML += newAbolethTemplate;
-  }
+    }
 }
 
 initAboleth();
