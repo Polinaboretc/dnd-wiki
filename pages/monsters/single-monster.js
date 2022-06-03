1e3f6ad4f242641d8ab13b46912d8a5515254bbc
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-06-03 16:20:35 +0200

    primo tentativo ti toggle searchbar
    toccati html, css, e js di single monster;
    la barra di ricerca non quadra  nello slider

--- pages/monsters/single-monster.js
+++ pages/monsters/single-monster.js
@@ -222,6 +222,16 @@ function createAccordionElement(infosArray, infoName){
         divAccordionContainer.innerHTML += newTemplate;
     }
     return divAccordionContainer;
-}
+} 
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "300px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
 
 init();
