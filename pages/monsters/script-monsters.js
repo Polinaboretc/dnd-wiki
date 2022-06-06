9b392aefbba493e9a11a6611e8454d1c5109e7b3
Frog <artisangiorgio98@gmail.com>
2022-06-06 21:51:11 +0200

    aggiunto flipping monster cards
    Manca ancora l'info dietro la carta e sarebbe ancora da modificare un pochino lo stile. Si ridimensionano dinamicamento e si girano on-hover. Per telefono non funziona ancora dato che non si può fare l'on-hover. Dovrò rendere l'intera carta un bottone

--- pages/monsters/script-monsters.js
+++ pages/monsters/script-monsters.js
@@ -15,21 +15,19 @@ function initMonsters() {
     });
 }
 
-function displayMonsters(monsters) { 
-
-  // Invece di scrivere tutto direttamente nel body, ho creato un contenitore apposito per i mostri
-  // Ciò renderà futura gestione estetica molto più semplice
-  const monstersContainer = document.getElementById("monsters-container");
-  // puliamo il contenitore
-  monstersContainer.innerHTML = "";
-  for (const monster of monsters) {
-    const div = document.createElement("div"); // Creo il div che conterrà il singolo mostro
-    div.className = "accordion-item single-monster-container";
-    div.innerHTML = createMonsterTemplate(monster); // Riempo il div con il template
-    const seeMoreButton = div.querySelector(".see-more");
-    seeMoreButton.onclick = () => goToMonsterPage(monster.index); // Aggiungo al bottone la funzione goToMonsterPage()
-    monstersContainer.appendChild(div); // Aggiungo il div del mostro singolo al div che contiene tutti i mostri
-  }
+function displayMonsters(monsters) {
+    const monstersContainer = document.getElementById("monsters-container");
+    monstersContainer.innerHTML = "";
+    for (const monster of monsters) {
+        const flipCardDiv = document.createElement("div"); // Creo il div che conterrà il singolo mostro
+        flipCardDiv.className = "flip-card";
+        flipCardDiv.innerHTML = createMonsterTemplate(monster)
+        const img = flipCardDiv.querySelector('img')
+        
+        const seeMoreButton = flipCardDiv.querySelector(".see-more");
+        seeMoreButton.onclick = () => goToMonsterPage(monster.index); // Aggiungo al bottone la funzione goToMonsterPage()
+        monstersContainer.appendChild(flipCardDiv); // Aggiungo il div del mostro singolo al div che contiene tutti i mostri
+    }
 }
 
 function search() {
@@ -48,55 +46,41 @@ function goToMonsterPage(index) {
   window.location.href = urlString;
 }
 
-initMonsters();
-
 function createMonsterTemplate(monster) {
-  // Ho cambiato un po' il metodo di mostrare i mostri.
-  // Immagini e nome non sono più dei link. Per andare alla pagina del singolo mostro
-  // bisognerà cliccare un bottone. Ho fatto ciò per rendere più facile la gestione
-  // in futuro dell'estetica. Manipolare bottoni e loro funzionalità
-  // è molto più facile rispetto ai link
-  const monstersTemplate = `
-    <h2 class="accordion-header" id="headingOne">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="##COLLAPSE_ID"  aria-controls="#COLLAPSE_ID">
-            <div class="accordion-monster-header">
-                <div>
-                    <img src ="#MONSTERSIMG" alt ="#MONSTERSALT" class="monsters-img">
-                    #MONSTERSNOME
+    const monsterCardTemplate = `
+    <div class="flip-card-inner">
+            <div class="flip-card-front">
+            <div class="img-container">
+            <img class="monster-img" src="#MONSTER_URL" onerror="./pictures/default.jpeg" alt="#MONSTER_ALT">
                 </div>
-                <a class="see-more btn btn-danger">+</a>
+                <div class="monster-name-container"><div>#MONSTERSNOME</div></div>
             </div>
-        </button>
+            <div class="flip-card-back">
+                <button class="see-more">+</button>
+            </div>
+        </div>`;
         
-        
-    </h2>
-    <div id="#COLLAPSE_ID" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-        </div>
-    </div>
-       `;
-  let monsterImg = "./pictures/" + monster.index + ".jpg";
-  if (
-    monster.index === "acolyte" ||
-    monster.index === "giant-poisonous-snake" ||
-    monster.index === "werebear-human" ||
-    monster.index === "werebear-hybrid" ||
-    monster.index === "wererat-human" ||
-    monster.index === "werebear-hybrid" ||
-    monster.index === "weretiger-human" ||
-    monster.index === "wereboar-hybrid" ||
-    monster.index === "wereboar-human" ||
-    monster.index === "wererat-hybrid" ||
-    monster.index === "weretiger-hybrid" ||
-    monster.index === "werewolf-hybrid"
-  ) {
-    monsterImg = "./pictures/default.jpeg";
-  }
-  return monstersTemplate
-    .replace("#MONSTERSIMG", monsterImg)
-    .replace("#MONSTERSALT", monster.index)
-    .replace("#COLLAPSE_ID", "collpase" + monster.index)
-    .replace("#COLLAPSE_ID", "collpase" + monster.index)
-    .replace("#COLLAPSE_ID", "collpase" + monster.index)
-    .replace("#MONSTERSNOME", monster.name);
+    let monsterUrl
+    if (
+        monster.index === "acolyte" ||
+        monster.index === "giant-poisonous-snake" ||
+        monster.index === "werebear-human" ||
+        monster.index === "werebear-hybrid" ||
+        monster.index === "wererat-human" ||
+        monster.index === "werebear-hybrid" ||
+        monster.index === "weretiger-human" ||
+        monster.index === "wereboar-hybrid" ||
+        monster.index === "wereboar-human" ||
+        monster.index === "wererat-hybrid" ||
+        monster.index === "weretiger-hybrid" ||
+        monster.index === "werewolf-hybrid"
+        ) {
+            monsterUrl = "./pictures/default.jpeg";
+    } else  monsterUrl = "./pictures/" + monster.index + ".jpg";
+    return monsterCardTemplate
+        .replace("#MONSTER_URL", monsterUrl)
+        .replace("#MONSTER_ALT", monster.index)
+        .replace("#MONSTERSNOME", monster.name);
 }
+
+initMonsters();
