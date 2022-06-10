599d7006dfad46890b113d3f77c9e7026fe975cf
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-06-10 12:49:01 +0200

    aut

--- pages/monsters/single-monster.js
+++ pages/monsters/single-monster.js
@@ -83,7 +83,7 @@ function autocomplete(inp, arr) {
     }); 
 
     inp.addEventListener("keydown", function(e){ 
-        const x = document.getElementById(this.id + "autocomplete-list"); 
+        let x = document.getElementById(this.id + "autocomplete-list"); 
         if (x) x = x.getElementsByTagName("div"); 
         if (e.keyCode == 40){ 
             currentFocus ++; 
@@ -305,7 +305,9 @@ function createAccordionElement(infosArray, infoName) {
 
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "290px";
+    document.getElementById("mySidepanel").style.width = "290px"; 
+    document.getElementById("open-button").style.zIndex = "0";
+    document.getElementById("close-button").style.zIndex = "-1";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
